ca17ea203b2f0717066c5391f9600511f59be700
slim-s <78751030+slim-s@users.noreply.github.com>
2022-03-02 15:33:33 +0530

    commit on main

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export const StyledButton = styled.button`
 `;
 
 export const StyledRoundButton = styled.button`
-  padding: 10px;
+  padding: 20px;
   border-radius: 100%;
   border: none;
   background-color: var(--primary);
@@ -57,10 +57,10 @@ export const ResponsiveWrapper = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column;
-  justify-content: stretched;
-  align-items: stretched;
-  width: 80%;
-  @media (min-width: 767px) {
+  justify-content: center;
+  align-items: center;
+  width: 60%;
+  @media (min-width: 60px) {
     flex-direction: row;
   }
 `;
@@ -196,28 +196,29 @@ function App() {
   return (
     <s.Screen>
       <s.Container
-          flex={2}
-            ai={"center"}
-            style={{ padding: 15, backgroundColor: "var(--primary)" }}
-            image={CONFIG.SHOW_BACKGROUND ? "/config/images/backdrop.png" : null}
-          >
-        
+        flex={3}
+        ai={"center"}
+        style={{ padding: 0, backgroundColor: "var(--primary)" }}
+        image={CONFIG.SHOW_BACKGROUND ? "/config/images/backdrop.png" : null}
+      >
         <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
         <s.SpacerLarge />
-        <ResponsiveWrapper  style={{ padding: 20 }} test>
-          <s.Container  jc={"center"} ai={"center"}>
+        <s.SpacerLarge />
+        <s.SpacerLarge />
+
+        <ResponsiveWrapper style={{ padding: 0 }} test>
+          <s.Container jc={"flex-start"} ai={"center"}>
             <StyledImg alt={"example"} src={"/config/images/example.gif"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
             flex={3}
-            jc={"top"}
-            ai={"top"}
+            jc={"center"}
+            ai={"center"}
             style={{
               backgroundColor: "var(--accent)",
-              padding: 24,
-              borderRadius: 10,
-              border: "4px dashed var(--secondary)",
+              padding: 20,
+              borderRadius: 20,
               boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
             }}
           >
@@ -283,7 +284,7 @@ function App() {
                         color: "var(--accent-text)",
                       }}
                     >
-                      Connect to the {CONFIG.NETWORK.NAME} network
+                      Connect to the {CONFIG.NETWORK.NAME}
                     </s.TextDescription>
                     <s.SpacerSmall />
                     <StyledButton
@@ -372,26 +373,234 @@ function App() {
           </s.Container>
           <s.SpacerLarge />
         </ResponsiveWrapper>
-        <s.SpacerMedium />
+        <s.SpacerLarge />
+
+        <s.Container jc={"center"} ai={"center"} style={{ width: "50%" }}>
+          <s.TextDescription
+            style={{
+              textAlign: "center",
+              color: "var(--primary-text)",
+              fontSize: 60,
+            }}
+          >
+            <h3>!WELCOME!</h3>
+          </s.TextDescription>
+          <s.SpacerSmall />
+          <s.TextDescription
+            style={{
+              textAlign: "center",
+              color: "var(--primary-text)",
+              fontSize: 20,
+            }}
+          >
+            Welcome to the Goat Nation, a club of 10,000 randomly generated
+            Goats, stored as ERC721 on Polygon blockchain and hosted on
+            Inter-Planetry File System(IPFS).<br></br>
+            !!HAPPY MINTING!!
+          </s.TextDescription>
+        </s.Container>
+        <s.SpacerLarge />
+        <s.Container jc={"center"} ai={"end"} style={{ width: "50%" }}>
+          <s.TextDescription
+            style={{
+              textAlign: "end",
+              color: "var(--primary)",
+              fontSize: 60,
+            }}
+          >
+            <h3>!STORY!</h3>
+          </s.TextDescription>
+        </s.Container>
+        <s.SpacerLarge />
+
         <s.Container jc={"center"} ai={"center"} style={{ width: "80%" }}>
-            <s.TextDescription
-              style={{
-                textAlign: "center",
-                color: "var(--primary-text)",
-              }}
+          <s.TextDescription
+            style={{
+              textAlign: "center",
+              color: "var(--primary)",
+              fontSize: 60,
+            }}
+          >
+           <h4>!TYPE OF DemiGOATS!</h4>
+          </s.TextDescription>
+          <s.SpacerLarge />
+          <s.TextDescription
+            style={{
+              textAlign: "center",
+              color: "var(--primary-text)",
+              fontSize:30,
+            }}
             >
-              Please make sure you are connected to the right network (
-              {CONFIG.NETWORK.NAME} Mainnet) and the correct address. 
-            </s.TextDescription>
-            <s.SpacerSmall />
-            <s.TextDescription
-              style={{
-                textAlign: "center",
-                color: "var(--primary-text)",
-              }}
-            >
-              Smart Contract:
+              DemiGoats &nbsp; GrimReaper &nbsp; TheEmperor
+
+
+
+
+          </s.TextDescription>
+        </s.Container>
+        <s.SpacerLarge />
+        <s.SpacerLarge />
+        <s.SpacerLarge />
+        <s.SpacerLarge />
+        <s.Container jc={"center"} ai={"center"} style={{ width: "50%" }}>
+          <s.TextDescription
+            style={{
+              textAlign: "center",
+              color: "var(--primary)",
+              fontSize: 60,
+            }}
+          >
+            <h4>!ROADMAP!</h4>
+          </s.TextDescription>
+        </s.Container>
+        <s.SpacerLarge />
+        <s.SpacerLarge />
+        <s.SpacerLarge />
+        <s.SpacerLarge />
+        <s.Container jc={"center"} ai={"start"} style={{ width: "70%" }}>
+          <s.TextDescription
+            style={{
+              textAlign: "center",
+              color: "var(--primary)",
+              fontSize: 40,
+            }}
+          >
+            <h5>The Launch!</h5>
+          </s.TextDescription>
+        </s.Container>
+        <s.Container jc={"center"} ai={"start"} style={{ width: "70%" }}>
+          <s.TextDescription
+            style={{
+              textAlign: "start",
+              color: "var(--primary)",
+              fontSize: 21,
+            }}
+          >
+            DemiGoats Pack will be unleashed on xx-xx-xx.
+            Only the whitelisted addresses <br></br>will be allowed 
+            to become a DemiGoat.
+            500 NFTs will be minted randomly <br></br> in 
+            the first round, among those, there 
+            might be some of the GrimReaper <br></br>
+            and The Emperor NFTs.
+             </s.TextDescription>
+        </s.Container>
+        <s.SpacerLarge />
+        <s.SpacerLarge />
+        <s.Container jc={"center"} ai={"start"} style={{ width: "70%" }}>
+          <s.TextDescription
+            style={{
+              textAlign: "center",
+              color: "var(--primary)",
+              fontSize: 40,
+            }}
+          >
+            <h5>Public availability!</h5>
+          </s.TextDescription>
+        </s.Container>              
+        <s.Container jc={"center"} ai={"start"} style={{ width: "70%" }}>
+          <s.TextDescription
+            style={{
+              textAlign: "start",
+              color: "var(--primary)",
+              fontSize: 21,
+            }}
+          >There will be a public launch of DemiGoats 
+           after the second round minting is<br></br> completed.
+          
             
+          </s.TextDescription>
+        </s.Container>
+        <s.SpacerLarge />
+        <s.SpacerLarge />
+        <s.SpacerLarge />
+        <s.SpacerLarge />
+        <s.Container jc={"center"} ai={"end"} style={{ width: "70%" }}>
+          <s.TextDescription
+            style={{
+              textAlign: "end",
+              color: "var(--primary)",
+              fontSize: 40,
+            }}
+          >
+            <h5>GrimReaper collection!</h5>
+          </s.TextDescription>
+        </s.Container>
+        <s.Container jc={"center"} ai={"center"} style={{ width: "80%" }}>
+          <s.TextDescription
+            style={{
+              textAlign: "center",
+              color: "var(--primary)",
+              fontSize: 60,
+            }}
+          >
+            <h5>!TEAM!</h5>
+          </s.TextDescription>
+          <s.SpacerSmall />
+          <s.TextDescription
+            style={{
+              textAlign: "center",
+              color: "var(--primary-text)",
+              fontSize: 30,
+            }}
+          >
+            1 &nbsp; 2&nbsp; 3 &nbsp;4&nbsp;5
+          </s.TextDescription>
+        </s.Container>
+
+        <s.SpacerLarge />
+
+        <s.Container jc={"center"} ai={"start"} style={{ width: "65%" }}>
+          <s.TextDescription
+            style={{
+              textAlign: "start",
+              color: "var(--primary)",
+              fontSize: 25,
+            }}
+          >
+            <h6>FAQs:</h6>
+            Where can I view my DemiGoat NFTs?
+            <br></br>
+            Once you have minted a DemiGoat NFT, you will be able to see it by
+            connecting you crypto-wallet.
+            <br></br>
+            When is the Presale event dated?
+            <br></br>
+            we will be holding a presale event on the date addressed above for
+            our Whitelist members. Please join our Discord to learn how you can
+            join the Whitelist. The Whitelist spots will be very limited.
+            <br></br>
+            What can I do with my DemiGoat ?<br></br>
+            You can use your DemiGoat as a profile picture online, and you can
+            also resell your DemiGoat for profit. Once Metavers is launched, you
+            will be able to use them in the Metavers and earn DemiGoat tokens.
+            <br></br>
+          </s.TextDescription>
+          <s.SpacerSmall />
+        </s.Container>
+
+        <s.SpacerLarge />
+        <s.Container jc={"center"} ai={"center"} style={{ width: "80%" }}>
+          <s.TextDescription
+            style={{
+              textAlign: "center",
+              color: "var(--primary-text)",
+              fontSize: 16,
+            }}
+          >
+            Please make sure you are connected to the right network (
+            {CONFIG.NETWORK.NAME} Mainnet) and the correct address.
+          </s.TextDescription>
+          <s.SpacerSmall />
+          <s.TextDescription
+            style={{
+              textAlign: "center",
+              color: "var(--primary-text)",
+              fontSize: 20,
+            }}
+          >
+            Smart Contract: <br></br>
+            0x83Cede07Ef0CEfE7e2bdd0B28F0936a6E5c43258
           </s.TextDescription>
         </s.Container>
       </s.Container>
